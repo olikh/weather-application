20bec541af42b5f43d979cb2bbc2166e24c9cfeb
Olena <61514299+olikh@users.noreply.github.com>
2022-08-12 12:25:11 +0300

    C and F removed css added

--- src/app.js
+++ src/app.js
@@ -37,7 +37,8 @@ function displayForecast(response) {
             <div class="weather-date">${formatDay(forecastDay.dt)}</div>
                 <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="">
             <div class="weather-forecast-temp">
-                <span class="weather-temperature">25°C</span>
+                <span class="weather-temperature-max">${Math.round(forecastDay.temp.max)}°</span>
+                <span class="weather-temperature-min">${Math.round(forecastDay.temp.min)}°</span>
         </div>
         </div>
       `
@@ -88,33 +89,9 @@ function handleSubmit(event) {
     let cityInputElement = document.querySelector("#city-input");
     search(cityInputElement.value);
 }
-function displayfahrenheitTemperature(event) {
-    event.preventDefault();
-    let temperatureElement = document.querySelector("#temperature");
-    celsiusLink.classList.remove("active");
-    fahrenheitLink.classList.add("active");
-    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
-
-function displayCelsiusTemperature(event) {
-    event.preventDefault();
-    celsiusLink.classList.add("active");
-    fahrenheitLink.classList.remove("active");
-    let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
-
-let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayfahrenheitTemperature);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Paris");
 
