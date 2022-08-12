2d6e188bd6930b2520a4b885fad705c778f9cf92
Olena <61514299+olikh@users.noreply.github.com>
2022-08-12 10:52:02 +0300

    forecast block added

--- src/app.js
+++ src/app.js
@@ -13,6 +13,33 @@ function formatDate(timestamp) {
     let day = days[date.getDay()];
 return `${day} ${hours}:${minutes}`;
 }
+function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+    let days = ["Thu", "Fri", "Sat", "Sun"];
+
+    let forecastHTML = '<div class="row">';
+    days.forEach(function (day){
+        forecastHTML = 
+        forecastHTML + 
+        `<div class="col-2">
+            <div class="weather-date">${day}</div>
+                <img src="images/sunny.png" class="cloud" alt="">
+            <div class="weather-forecast-temp">
+                <span class="weather-temperature">25°C</span>
+        </div>
+        </div>
+      `
+    });
+    forecastHTML = forecastHTML + `</div>`
+    forecastElement.innerHTML = forecastHTML;
+}
+function getForecast(coordinates) {
+    console.log(coordinates);
+    let apiKey = "ef0245cf2c991c7cb4169246b7a9ad19";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(displayForecast);
+}
 
 function displayTemperature(response){
     console.log(response.data.main.temp);
@@ -34,6 +61,8 @@ function displayTemperature(response){
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
     iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt", response.data.weather[0].description);
+    
+    getForecast(response.data.coord)
 }
 
 function search(city) {
@@ -76,4 +105,5 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Paris");
+displayForecast();
 
